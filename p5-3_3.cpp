c51837c32adef4f4e07e42ebcb8f80c36c5c1fa6
py-v <yugpvasani.ce@gmail.com>
2026-08-11 10:47:13 +0530

    Add files via upload

---
+++ p5-3_3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+    {
+
+        char i,j,n;
+
+
+        cin>>n;
+        for(i=65;i<=n+1;i++)
+        {
+            for(j=65;j<=i;j++)
+            {
+                cout<<j;
+
+
+            }
+            cout<<endl;
+
+        }
+        return 0;
+    }
+
+
+
+
+
