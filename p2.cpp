4c00f2b2107145c1038775dbd4abdca0f724a6d0
py-v <yugpvasani.ce@gmail.com>
2026-08-22 02:46:32 +0530

    Add files via upload

---
+++ p2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sem,n;
+    string en,name,branch;
+
+    cout<<"**********************************"<<endl;
+    cout<<" STUDENT RECORDMANAGEMENT SYSTEM  "<<endl;
+    cout<<"**********************************"<<endl;
+
+    cout<<"Enter Enrollment Number :";
+    cin>>en;
+    cout<<endl<<"Enter Student Name :";
+    cin>>name;
+    cout<<endl<<"Branch :";
+    cin>>branch;
+    cout<<endl<<"ENter semester :";
+    cin>>sem;
+    cout<<endl<<"Enter Mobile Number :";
+    cin>>n;
+    cout<<endl;
+    cout<<"-----------------------"<<endl;
+    cout<<"  Student Information  "<<endl;
+    cout<<"-----------------------"<<endl;
+    cout<<"Enrollment Number :"<<en<<endl;
+    cout<<"Student Name :"<<name<<endl;
+    cout<<"Branch :"<<branch<<endl;
+    cout<<"semester :"<<sem<<endl;
+    cout<<"Mobile Number :"<<n<<endl;
+    cout<<"------------------------"<<endl;
+}
